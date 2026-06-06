a6cb9bd9dfd0a5b27f95ca69d6e7c4299ff39099
gall0ws <g4ll0ws@gmail.com>
2026-06-06 04:13:54 +0200

    allergies.el: more idiomatic name for the constant
    +constant+ is more a common-lisp thing

--- emacs-lisp/allergies/allergies.el
+++ emacs-lisp/allergies/allergies.el
@@ -3,24 +3,26 @@
 ;;; Commentary:
 
 ;;; Code:
-(defconst +allergy-scores+ '(("eggs"            . 1)
-                             ("peanuts"         . 2)
-                             ("shellfish"       . 4)
-                             ("strawberries"    . 8)
-                             ("tomatoes"        . 16)
-                             ("chocolate"       . 32)
-                             ("pollen"          . 64)
-                             ("cats"            . 128)))
+(defconst allergies--allergens-alist
+  '(("eggs"            . 1)
+    ("peanuts"         . 2)
+    ("shellfish"       . 4)
+    ("strawberries"    . 8)
+    ("tomatoes"        . 16)
+    ("chocolate"       . 32)
+    ("pollen"          . 64)
+    ("cats"            . 128)))
 
 (defun allergen-list (score)
   (seq-filter (lambda (allergen)
                 (allergic-to-p score allergen))
-              (mapcar 'car +allergy-scores+)))
+              (mapcar 'car allergies--allergens-alist)))
 
 (defun allergic-to-p (score allergen)
   (not (zerop (logand
                score
-               (or (cdr-safe (assoc-string allergen +allergy-scores+)) 0)))))
+               (or (cdr-safe (assoc-string allergen allergies--allergens-alist))
+                   0)))))
 
 (provide 'allergies)
 ;;; allergies.el ends here
