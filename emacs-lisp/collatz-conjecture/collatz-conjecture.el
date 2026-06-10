0d86604811a2167126f479c9c7de3736746b9182
gall0ws <g4ll0ws@gmail.com>
2026-06-10 19:23:49 +0200

    emacs-lisp/collatz-conjecture: replaced cl-labels with named-let

--- emacs-lisp/collatz-conjecture/collatz-conjecture.el
+++ emacs-lisp/collatz-conjecture/collatz-conjecture.el
@@ -6,14 +6,12 @@
 (defun collatz (number)
   "Produce the Collatz sequence starting from NUMBER."
   (when (< number 1) (error "collatz is defined only for positive integers"))
-  (cl-labels
-      ((rec (lst)
-         (pcase (car lst)
-           (1 lst)
-           ((pred cl-evenp)
-            (rec (cons (/ (car lst) 2) lst)))
-           (_ (rec (cons (1+ (* 3 (car lst))) lst))))))
-    (nreverse (rec (list number)))))
+  (named-let rec ((lst (list number)))
+    (pcase (car lst)
+      (1 (nreverse lst))
+      ((pred cl-evenp)
+       (rec (cons (/ (car lst) 2) lst)))
+      (_ (rec (cons (1+ (* 3 (car lst))) lst))))))
 
 (defun steps (number)
   "Count the steps to reach 1 using the Collatz conjecture."
